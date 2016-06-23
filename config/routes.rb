ca4fc13474b52f46ff1696e0d3b46ba82c1f3812
Kentaro <k.kuretani@gmail.com>
2016-06-23 17:42:12 +0000

    homework

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
   get    'signup', to: 'users#new'
+  get    'edit'  , to: 'users#edit'
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy, :show, :edit, :update]
+  resources :users, only: [:edit] do
+    collection do
+      patch 'update_password'
+    end
+  end
 end
