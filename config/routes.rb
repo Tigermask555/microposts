d3278b3bc481076e460239d755d65de05570153e
Kentaro <k.kuretani@gmail.com>
2016-06-25 17:49:26 +0000

    homework1-2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
   get    'signup', to: 'users#new'
   get    'edit'  , to: 'users#edit'
+  get    'show'  , to: 'users#show'
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
