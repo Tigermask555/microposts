caa0271c806931ce9e009a4344abc1f54a5a422c
Kentaro <k.kuretani@gmail.com>
2016-06-30 13:21:58 +0000

    homework2-7

--- db/migrate/20160624160321_create_relationships.rb
+++ db/migrate/20160624160321_create_relationships.rb
@@ -5,11 +5,9 @@ class CreateRelationships < ActiveRecord::Migration
       t.references :followed, index: true
       
       t.timestamps null: false
-      t.index [:user_id, :created_at]
+      t.index [:follower_id, :followed_id], unique: true
      
     end
-    add_index :relationships, :follower_id
-    add_index :relationships, :followed_id
-    add_index :relationships, [:follower_id, :followed_id], unique: true
+  
   end
 end
